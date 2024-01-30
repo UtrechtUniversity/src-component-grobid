8c80985a3b19e1e546d7104a47a76f58c4f025eb
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-01-30 18:30:51 +0100

    Add script for creating nginx reverse proxy

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat << EOF > /etc/nginx/app-location-conf.d/grobid.conf
+location /grobid {
+    rewrite /grobid/(.*) /$1 break;
+    proxy_read_timeout  90s;
+    proxy_connect_timeout 300;
+    proxy_send_timeout 300;
+    send_timeout 300;
+    client_max_body_size 100M;
+
+    proxy_set_header   X-Forwarded-For $remote_addr;
+    proxy_set_header   Host $host;
+    proxy_pass http://localhost:8070/;
+}
+EOF
+
+systemctl restart nginx
