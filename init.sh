146341ca3bcf67b173c37e9d1c0642c163892cfb
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-01-31 10:29:26 +0100

    Add systemd service

--- init.sh
+++ init.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+compose_location="$( dirname -- "${BASH_SOURCE[0]}" )/docker-compose.yml"
+
+cat <<EOF > /etc/systemd/system/grobid.service
+[Unit]
+Description=Grobid Service
+Requires=docker.service
+After=docker.service
+StartLimitIntervalSec=60
+
+[Service]
+WorkingDirectory=$compose_location
+ExecStart=/usr/local/bin/docker-compose up
+ExecStop=/usr/local/bin/docker-compose down
+TimeoutStartSec=0
+Restart=on-failure
+StartLimitBurst=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable grobid.service
+
 cat << EOF > /etc/nginx/app-location-conf.d/grobid.conf
 location /grobid/ {
     proxy_read_timeout  90s;
