3fd70a6fc50a43b7c1354672c45943bd078a092a
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-03-13 11:14:26 +0100

    Update init.sh
    Add datastet and softcite

--- init.sh
+++ init.sh
@@ -37,4 +37,32 @@ location /grobid/ {
 }
 EOF
 
+cat << EOF > /etc/nginx/app-location-conf.d/datastet.conf
+location /datastet/ {
+    proxy_read_timeout  90s;
+    proxy_connect_timeout 300;
+    proxy_send_timeout 300;
+    send_timeout 300;
+    client_max_body_size 100M;
+
+    proxy_set_header X-Forwarded-For \$remote_addr;
+    proxy_set_header Host \$host;
+    proxy_pass http://localhost:8060/;
+}
+EOF
+
+cat << EOF > /etc/nginx/app-location-conf.d/softcite.conf
+location /softcite/ {
+    proxy_read_timeout  90s;
+    proxy_connect_timeout 300;
+    proxy_send_timeout 300;
+    send_timeout 300;
+    client_max_body_size 100M;
+
+    proxy_set_header X-Forwarded-For \$remote_addr;
+    proxy_set_header Host \$host;
+    proxy_pass http://localhost:8061/;
+}
+EOF
+
 systemctl restart nginx
