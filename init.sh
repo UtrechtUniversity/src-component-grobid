33c2149dc33676d1eada7f1d33d095b4f91e21fa
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-03-21 16:33:36 +0100

    Update init.sh
    Configure reverse proxies in a separate component.
    Fix oauth callback for reverse proxies at a location under /.

--- init.sh
+++ init.sh
@@ -23,46 +23,5 @@ EOF
 
 systemctl enable grobid.service
 
-cat << EOF > /etc/nginx/app-location-conf.d/grobid.conf
-location /grobid/ {
-    proxy_read_timeout  90s;
-    proxy_connect_timeout 300;
-    proxy_send_timeout 300;
-    send_timeout 300;
-    client_max_body_size 100M;
-
-    proxy_set_header X-Forwarded-For \$remote_addr;
-    proxy_set_header Host \$host;
-    proxy_pass http://localhost:8070/;
-}
-EOF
-
-cat << EOF > /etc/nginx/app-location-conf.d/datastet.conf
-location /datastet/ {
-    proxy_read_timeout  90s;
-    proxy_connect_timeout 300;
-    proxy_send_timeout 300;
-    send_timeout 300;
-    client_max_body_size 100M;
-
-    proxy_set_header X-Forwarded-For \$remote_addr;
-    proxy_set_header Host \$host;
-    proxy_pass http://localhost:8060/;
-}
-EOF
-
-cat << EOF > /etc/nginx/app-location-conf.d/softcite.conf
-location /softcite/ {
-    proxy_read_timeout  90s;
-    proxy_connect_timeout 300;
-    proxy_send_timeout 300;
-    send_timeout 300;
-    client_max_body_size 100M;
-
-    proxy_set_header X-Forwarded-For \$remote_addr;
-    proxy_set_header Host \$host;
-    proxy_pass http://localhost:8061/;
-}
-EOF
-
-systemctl restart nginx
+# Fix for oauth with reverse proxies at a subpath
+sed -i 's/oauth2_callback&/oauth2_callback?rdpath=$request_uri\&/' /etc/nginx/app-location-conf.d/authentication.conf
