05ad448a561760d2bbeb6421275710cd02d04175
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-03-25 10:53:12 +0100

    Revert "Define reverse proxies in script while Reverse Components scripts waits for SURF bugfix"
    This reverts commit fe6463643419f8b76aefbbc5a7f48d4801bb8bb1.

--- init.sh
+++ init.sh
@@ -26,23 +26,3 @@ systemctl enable grobid.service
 # Fix for oauth with reverse proxies at a subpath
 sed -i 's/oauth2_callback&/oauth2_callback?rdpath=$request_uri\&/' /etc/nginx/app-location-conf.d/authentication.conf
 sed -i 's/return 302 $scheme:\/\/$http_host\//return 302 $scheme:\/\/$http_host$arg_rdpath/' /etc/nginx/app-location-conf.d/authentication.conf
-
-VHOST_TEMPLATE="
-location %s {
-    proxy_read_timeout  90s;
-    proxy_connect_timeout 300;
-    proxy_send_timeout 300;
-    send_timeout 300;
-    client_max_body_size 100M;
-
-    proxy_set_header X-Forwarded-For \$remote_addr;
-    proxy_set_header Host \$host;
-    proxy_pass %s;
-}
-"
-
-printf "$VHOST_TEMPLATE" "/grobid/" "http://localhost:8070/" > /etc/nginx/app-location-conf.d/grobid.conf
-printf "$VHOST_TEMPLATE" "/datastet/" "http://localhost:8060" > /etc/nginx/app-location-conf.d/grobid.conf
-printf "$VHOST_TEMPLATE" "/softcite/" "http://localhost:8061" > /etc/nginx/app-location-conf.d/grobid.conf
-
-systemctl restart nginx
